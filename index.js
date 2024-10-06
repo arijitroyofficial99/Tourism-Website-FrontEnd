a948cc30a0858ce698933711fa8400fe0c998d73
Arijit <arijitroyofficial99@gmail.com>
2024-10-07 05:16:34 +0530

    frist commit

---
+++ index.js
@@ -0,0 +1,78 @@
+// chagne the visitbility fucntion of password
+
+function change_visibility()
+{
+    object=document.getElementById("password");
+
+    if (object.type==="password"){
+        object.type="text"
+    
+    }
+    else{object.type="password"}
+}
+
+let formbtn = document.querySelector("#account_icon");
+let closeform = document.querySelector("#close_form");
+let loginform = document.querySelector(".login_form");
+
+let menu_bar=document.querySelector("#menu-bar");
+let nav_section=document.querySelector(".nav_section")
+
+
+let videobtn=document.querySelectorAll(".video-btn");
+
+
+
+
+menu_bar.addEventListener("click", () => {
+    menu_bar.classList.toggle("fa-times")
+    nav_section.classList.toggle("active");
+
+});
+
+
+
+formbtn.addEventListener("click", () => {
+    loginform.classList.add("active");
+    
+});
+
+closeform.addEventListener("click", () => {
+    loginform.classList.remove("active");
+});
+
+
+videobtn.forEach(btn => {
+
+    btn.addEventListener("click",()=>{
+        document.querySelector(".controls .active").classList.remove("active");
+        btn.classList.add("active");
+        let temp=btn.getAttribute("data-src");
+        document.querySelector("#video-slider").src = temp;
+
+
+    });
+    
+});
+
+
+// book section javascript
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
